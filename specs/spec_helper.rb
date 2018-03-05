c547e7b674414c2a0e20fff457c1d35cc3662dfd
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-05 15:38:18 -0800

    create spec helper

---
+++ specs/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+require 'time'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative '../lib/hotel_admin'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
