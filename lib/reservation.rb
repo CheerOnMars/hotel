509fef86d188dd703e91c0563785e6cf0602fbd7
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-07 09:47:37 -0800

    add room to initialize method in reservation class, add self method for start and end dates

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -17,14 +17,38 @@ module Hotel
         raise ArgumentError.new("End date must be after start date.")
       end
 
+      #add room_id ArgumentError if necessary
+
       @start_date = input[:start_date]
       @end_date = input[:end_date]
+      @room_id = input[:room_id]
 
     end
 
     def length
       ((@end_date - @start_date) / 86400).to_i
     end
+
+    # def puts_self
+    #   puts Hotel::Reservation.new
+    # end
+
+    def start_date
+      @start_date
+    end
+
+    def end_date
+      @end_date
+    end
+
+    #.strftime("%m/%d/%Y")
+
+    # def self.all
+    #   reservations_arr = []
+    #   @reservation
+    #   reservations_arr << reservation
+    #   return reservations_arr
+    # end
 
   end
 end
