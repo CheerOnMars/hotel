45907e87ccca09db0d444b83ca1517382ef224ec
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-06 18:21:40 -0800

    updated filed names so they could be read by rake

---
+++ specs/room_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'spec_helper'
+
+describe "Room class" do
+
+  describe "Initializer" do
+    it "Can be created" do
+      room = Hotel::Room.new("unit_name")
+      room.must_be_instance_of Hotel::Room
+    end
+
+    it "Is an instance of Room" do
+      room = Hotel::Room.new("unit_name")
+      room.class.must_equal Hotel::Room
+    end
+
+  end
+
+  describe "price method" do
+    it "returns price" do
+      room = Hotel::Room.new("unit_name")
+      room.price.must_equal 200
+      room.price.must_be_kind_of Integer
+    end
+  end
+
+end
