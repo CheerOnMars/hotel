474e97da9de47f31a75c393b97d17463ba4cade6
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-11 21:55:37 -0700

    added lib/block and removed lib/room as required relatives

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -9,4 +9,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative '../lib/hotel_admin'
 require_relative '../lib/reservation'
-require_relative '../lib/room'
+require_relative '../lib/block'
