9c6e3e475907596ff3cd8642bcfb736a020eb016
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-12 00:20:14 -0700

    updated HotelAdmin class and its tests to include block functionality

--- lib/hotel_admin.rb
+++ lib/hotel_admin.rb
@@ -3,17 +3,18 @@ require_relative 'reservation'
 
 module Hotel
   class HotelAdmin
-    attr_reader  :rooms, :reservations, :reservation
+    attr_reader  :rooms, :reservations, :reservation, :block_start_date, :block_end_date, :room_amt, :blocks, :block
 
     def initialize
       @rooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
       @reservations = []
+      @blocks = []
     end
 
     def add_reservation(reservation)
       #note: this is tightly coupled, I think.
       @reservations << reservation
-    #  @reservation = reservation
+      @reservation = reservation
       return @reservation
     end
 
@@ -48,6 +49,38 @@ module Hotel
       avail_room = view_available_rooms(checkin_request, checkout_request)[0]
       add_reservation(Hotel::Reservation.new({checkin_date: checkin_request, checkout_date: checkout_request, room_id: avail_room}))
     end
+
+
+    def create_block(input)
+
+      if input[:block_start_date] == nil || input[:block_end_date] == nil
+        raise ArgumentError.new("Date must be a date.")
+      end
+
+      if input[:block_start_date] >= input[:block_end_date]
+        raise ArgumentError.new("End date must be after start date.")
+      end
+
+      if input[:room_amt] > 5 || input[:room_amt] < 1
+        raise ArgumentError.new("Rooms must be between 1 and 5.")
+      end
+
+      if input[:room_amt] > 5 || input[:room_amt] < 1
+        raise ArgumentError.new("Rooms must be between 1 and 5.")
+      end
+
+      if input[:room_amt] > view_available_rooms(input[:block_start_date], input[:block_end_date]).count
+        raise ArgumentError.new("There are not enough rooms available during that time.")
+      end
+
+      @block_start_date = input[:block_start_date]
+      @block_end_date = input[:block_end_date]
+      @room_num = input[:room_amt]
+      @blocks << input
+      @block = input
+      return @block
+    end
+
 
   end
 end
--- specs/hotel_admin_spec.rb
+++ specs/hotel_admin_spec.rb
@@ -2,41 +2,46 @@ require_relative 'spec_helper'
 
 describe "HotelAdmin class" do
   describe "Initializer" do
-    it "is an instance of TripDispatcher" do
-      hotel_admin = Hotel::HotelAdmin.new
-      hotel_admin.must_be_kind_of Hotel::HotelAdmin
+    before do
+      @hotel_admin = Hotel::HotelAdmin.new
+    end
+
+    it "is an instance of HotelAdmin" do
+      @hotel_admin.must_be_kind_of Hotel::HotelAdmin
     end
 
     it "establishes the base data structures when instantiated" do
-      hotel_admin = Hotel::HotelAdmin.new
       [:rooms, :reservations].each do |i|
-        hotel_admin.must_respond_to i
+        @hotel_admin.must_respond_to i
       end
-      hotel_admin.rooms.must_be_kind_of Array
-      hotel_admin.reservations.must_be_kind_of Array
+      @hotel_admin.rooms.must_be_kind_of Array
+      @hotel_admin.reservations.must_be_kind_of Array
     end
 
     it "lists of all of the rooms in the hotel" do
-      hotel_admin = Hotel::HotelAdmin.new
-      hotel_admin.rooms.must_equal [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+      @hotel_admin.rooms.must_equal [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+      @hotel_admin.rooms.first.must_equal 1
+      @hotel_admin.rooms.last.must_equal 20
     end
   end
 
   describe "dates reservations method" do
+    before do
+      @hotel_admin = Hotel::HotelAdmin.new
+    end
+
     it "can reserve a room for a given date range" do
-      hotel_admin = Hotel::HotelAdmin.new
-      new_res = hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
-      hotel_admin.reservations.must_include new_res
+      new_res = @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
+      @hotel_admin.reservations.must_include new_res
     end
 
     it "returns a list of reservations for a specific date" do
-      hotel_admin = Hotel::HotelAdmin.new
-      reservation_a = hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
-      reservation_b = hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2}))
-      hotel_admin.access_reservations("2015-07-21").must_equal [reservation_a]
-      hotel_admin.access_reservations("2015-07-22").must_equal [reservation_a, reservation_b]
-      hotel_admin.access_reservations("2015-07-19").must_equal []
-      hotel_admin.access_reservations("2015-07-25").must_equal []
+      reservation_a = @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
+      reservation_b = @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2}))
+      @hotel_admin.access_reservations("2015-07-21").must_equal [reservation_a]
+      @hotel_admin.access_reservations("2015-07-22").must_equal [reservation_a, reservation_b]
+      @hotel_admin.access_reservations("2015-07-19").must_equal []
+      @hotel_admin.access_reservations("2015-07-25").must_equal []
     end
   end
 
@@ -71,4 +76,81 @@ describe "HotelAdmin class" do
     end
   end
 
+  describe "creates blocks" do
+    before do
+      @hotel_admin = Hotel::HotelAdmin.new
+    end
+
+    #@block = Hotel::Block.new({block_start_date: "2015-07-20", block_end_date: "2015-07-25", room_amt: 5})
+
+    it "returns an error if block attempt has more than 5 rooms" do
+      proc{ @hotel_admin.create_block({block_start_date: "2015-07-20", block_end_date: "2015-07-25", room_amt: 6})}.must_raise ArgumentError
+    end
+
+    it "returns an error if block attempt has more than 5 rooms" do
+      proc{ @hotel_admin.create_block({block_start_date: "2015-07-20", block_end_date: "2015-07-25", room_amt: 6})}.must_raise ArgumentError
+    end
+
+    it "returns an error if block attempt has less than 1 room" do
+      proc{ @hotel_admin.create_block({block_start_date: "2015-07-20", block_end_date: "2015-07-25", room_amt: 0})}.must_raise ArgumentError
+    end
+
+    it "throws an argument error for a bad end date" do
+      proc{ @hotel_admin.create_block({block_start_date: "2015-07-20", block_end_date: nil }) }.must_raise ArgumentError
+    end
+
+    it "throws an argument error for a bad start date" do
+      proc{ @hotel_admin.create_block({block_start_date: nil, block_end_date: "2015-07-20", room_amt: 3 }) }.must_raise ArgumentError
+    end
+
+    it "throws an argument error when end date isn't after start date" do
+      proc{ @hotel_admin.create_block({block_start_date: "2015-07-20", block_end_date: "2015-07-20", room_amt: 3 }) }.must_raise ArgumentError
+      proc{ @hotel_admin.create_block({block_start_date: "2015-07-21", block_end_date: "2015-07-20", room_amt: 3 }) }.must_raise ArgumentError
+    end
+
+    it "can create a block of rooms" do
+      block_new = @hotel_admin.create_block({block_start_date: "2015-07-26", block_end_date: "2015-07-29", room_amt: 4})
+      block_newb = @hotel_admin.create_block({block_start_date: "2015-07-20", block_end_date: "2015-07-25", room_amt: 3})
+      block_new.must_be_instance_of Hash
+      @hotel_admin.blocks.must_be_instance_of Array
+    end
+
+
+    it "throws an argument error when there aren't enough rooms available" do
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 2}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 3}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 4}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 5}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 6}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 7}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 8}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 9}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 10}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 11}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 12}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 13}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 14}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 15}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 16}))
+      @hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 17}))
+      proc{ @hotel_admin.create_block({block_start_date: "2015-07-23", block_end_date: "2015-07-29", room_amt: 5}) }.must_raise ArgumentError
+    end
+
+
+    #
+    # it "only includes rooms that are available for the given date range" do
+    #
+    # end
+    #
+    # it "excludes rooms in the block for reservations by general public" do
+    # end
+    #
+    # it "can check whether a given block has any rooms available" do
+    # end
+    #
+    # it "can reserve a room from within a block of rooms" do
+    # end
+
+  end
 end
