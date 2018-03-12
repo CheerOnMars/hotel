21fbb57412061fe7dba1df2f850373720452ceec
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-11 21:45:32 -0700

    trimmed down the tests for the HotelAdmin class's methods

--- specs/hotel_admin_spec.rb
+++ specs/hotel_admin_spec.rb
@@ -25,17 +25,14 @@ describe "HotelAdmin class" do
   describe "dates reservations method" do
     it "can reserve a room for a given date range" do
       hotel_admin = Hotel::HotelAdmin.new
-      reservation_a = Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1})
-      new_res = hotel_admin.add_reservation(reservation_a)
+      new_res = hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
       hotel_admin.reservations.must_include new_res
     end
 
     it "returns a list of reservations for a specific date" do
       hotel_admin = Hotel::HotelAdmin.new
-      reservation_a = Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1})
-      reservation_b = Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2})
-      hotel_admin.add_reservation(reservation_a)
-      hotel_admin.add_reservation(reservation_b)
+      reservation_a = hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
+      reservation_b = hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2}))
       hotel_admin.access_reservations("2015-07-21").must_equal [reservation_a]
       hotel_admin.access_reservations("2015-07-22").must_equal [reservation_a, reservation_b]
       hotel_admin.access_reservations("2015-07-19").must_equal []
@@ -46,7 +43,7 @@ describe "HotelAdmin class" do
   describe "calculates reservation cost" do
     it "calculates the cost of a given reservation" do
       hotel_admin = Hotel::HotelAdmin.new
-      reservation = Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1})
+      reservation = hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
       hotel_admin.calc_reservations(reservation).must_equal 1000
       hotel_admin.calc_reservations(reservation).must_be_instance_of Integer
     end
@@ -55,10 +52,8 @@ describe "HotelAdmin class" do
   describe "can view a list of rooms that are not reserved for a given date range" do
     it "can test 1" do
       hotel_admin = Hotel::HotelAdmin.new
-      reservation_a = Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1})
-      reservation_b = Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2})
-      hotel_admin.add_reservation(reservation_a)
-      hotel_admin.add_reservation(reservation_b)
+      hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
+      hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2}))
       hotel_admin.view_available_rooms("2015-07-20", "2015-07-23").must_equal [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     end
   end
@@ -66,16 +61,13 @@ describe "HotelAdmin class" do
   describe "can reserve an available room for a given date range" do
     it "" do
       hotel_admin = Hotel::HotelAdmin.new
-      reservation_a = Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1})
-      reservation_b = Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2})
-      hotel_admin.add_reservation(reservation_a)
-      hotel_admin.add_reservation(reservation_b)
+      hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-20", checkout_date: "2015-07-25", room_id: 1}))
+      hotel_admin.add_reservation(Hotel::Reservation.new({checkin_date: "2015-07-22", checkout_date: "2015-07-24", room_id: 2}))
       new_res = hotel_admin.reserve_available_room("2015-07-20", "2015-07-23")
       new_res.must_be_kind_of Hotel::Reservation
       new_res.checkin_date.must_equal "2015-07-20"
       new_res.checkout_date.must_equal "2015-07-23"
       new_res.room_id.must_equal 3
-
     end
   end
 
