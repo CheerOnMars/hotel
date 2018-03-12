de23dda9df2cd97aa00ef4558da322c55156dd6a
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-11 19:15:05 -0700

    In HotelAdmin class, added method to view available rooms, and changed access_reservation method to remove strftime(%Y-%m-%d) functionality

--- lib/hotel_admin.rb
+++ lib/hotel_admin.rb
@@ -12,6 +12,7 @@ module Hotel
     end
 
     def add_reservation(reservation)
+      #note: this is tightly coupled, I think.
       @reservations << reservation
       @reservation = reservation
       return @reservation
@@ -24,14 +25,31 @@ module Hotel
     def access_reservations(day)
       days_reservations = []
       @reservations.each do |reservation|
-        checkin_date_local = reservation.checkin_date.strftime("%Y-%m-%d")
-        checkout_date_local = reservation.checkout_date.strftime("%Y-%m-%d")
-        if (checkin_date_local...checkout_date_local).include?(day)
+        # checkin_date_local = reservation.checkin_date.strftime("%Y-%m-%d")
+        # checkout_date_local = reservation.checkout_date.strftime("%Y-%m-%d")
+        if (reservation.checkin_date...reservation.checkout_date).include?(day)
           days_reservations << reservation
         end
       end
       return days_reservations
     end
+
+    def view_available_rooms(checkin_request, checkout_request)
+      arr_available_rooms = @rooms.map{ |room| room }
+      date_range_requested = (checkin_request...checkout_request).map{ |date| date}
+      @reservations.each do |res|
+        date_range_requested.each do |day_requested|
+          if (res.checkin_date...res.checkout_date).include?(day_requested)
+            arr_available_rooms.delete(res.room_id)
+          end
+        end
+      end
+      return arr_available_rooms
+    end
+
+    # def reserve_available_room(checkin_request, checkout_request)
+    #
+    # end
 
   end
 end
